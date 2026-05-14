d157ceccf90cf70f2603a006fae74e8c3bf5dbbd
JHON PARCO <Jhon.Parco-BLUEPARTNER@farmaciasperuanas.pe>
2026-05-14 15:25:59 -0500

    cambio dominio

--- src/assets/global/variable-config.ts
+++ src/assets/global/variable-config.ts
@@ -2,7 +2,7 @@ import { ConsentChannel } from "../../app/core/models/consent.models";
 
 export const variablesConfig = {
   production: true,
-  apiBaseUrl: 'https://welfare-lane-models-assured.trycloudflare.com/api/v1',
+  apiBaseUrl: 'https://western-farmers-continues-tokyo.trycloudflare.com/api/v1',
   brandName: 'DERMO PRIME / VISIA',
 
   accessCode: 'QR-MIFARMA-001',
